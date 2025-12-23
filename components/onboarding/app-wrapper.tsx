0d1e4eafb7f50a61a3d75cfc5d9d6b6ede31dcd0
nathidlamini445-cmd <nathidlamini445@gmail.com>
2025-12-23 23:38:06 +0200

    Delete components directory

--- components/onboarding/app-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { OnboardingGuard } from './onboarding-guard'
-
-export function AppWrapper({ children }: { children: React.ReactNode }) {
-  return <OnboardingGuard>{children}</OnboardingGuard>
-}
-
