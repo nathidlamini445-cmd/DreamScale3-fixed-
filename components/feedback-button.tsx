f4c9ab96245dbde4b5fa875be9c861ad2182ed7e
nathidlamini445-cmd <nathidlamini445@gmail.com>
2026-01-22 13:25:06 +0200

    Add files via upload

---
+++ components/feedback-button.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+// Feedback button is now integrated into the sidebar
+// This component is kept for backward compatibility but won't render
+export function FeedbackButton() {
+  return null // Feedback button is now in sidebar-nav.tsx
+}
+
