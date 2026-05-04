755bdbdb2cf717cfb6d0572aec6eaf178fb24fa4
nathidlamini445-cmd <nathidlamini445@gmail.com>
2026-05-04 18:52:44 +0200

    Delete components directory

--- components/feedback-button.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-// Feedback button is now integrated into the sidebar
-// This component is kept for backward compatibility but won't render
-export function FeedbackButton() {
-  return null // Feedback button is now in sidebar-nav.tsx
-}
-
